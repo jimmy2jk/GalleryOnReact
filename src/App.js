b5607e06bba6979d4bf9d6df9cf763ab77499407
jimmy2jk <zhekatkach04@gmail.com>
2023-11-23 17:31:56 +0200

    Finished work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Gallery from './components/Gallery';
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+import img4 from './images/img4.jpg';
+import img5 from './images/img5.jpg';
+import img6 from './images/img6.jpg';
+import img7 from './images/img7.jpg';
+
+const images = [
+  {
+    id: 1,
+    imageSrc: img1
+  },
+  {
+    id: 2,
+    imageSrc: img2
+  },
+  {
+    id: 3,
+    imageSrc: img3
+  },
+  {
+    id: 4,
+    imageSrc: img4
+  },
+  {
+    id: 5,
+    imageSrc: img5
+  },
+  {
+    id: 6,
+    imageSrc: img6
+  },
+  {
+    id: 7,
+    imageSrc: img7
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='app-header'>My Gallery</h1>
+      <Gallery images={images}/>
     </div>
   );
 }
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Gallery(props) {
+  const [isOpened, setIsOpened] = useState(false);
+  const [openedImg, setOpenedImg] = useState("");
+
+  const getImage = (imageSrc) => {
+    setOpenedImg(imageSrc);
+    setIsOpened(true);
+  };
+
+  return (
+    <>
+      <div className={isOpened ? "model opened" : "model"}>
+        <img src={openedImg} alt="Currently opened image" />
+        <div className="close-icon" onClick={() => setIsOpened(false)}>x</div>
+      </div>
+      <div className="gallery">
+        {props.images.map((item, index) => {
+          return (
+            <div
+              className="photo"
+              onClick={() => getImage(item.imageSrc)}
+              key={index}
+            >
+              <img src={item.imageSrc} alt={item.id} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
